47612532f474c3d0305cd536658f8329243b59cd
Arthur Weale <ajayrossy340@gmail.com>
2025-07-02 20:43:23 +0200

    Challenge 2: Create and Use useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 //import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
 
+
+
 function App() {
+  const [message, setMessage] = useState(`Let's build something cool today!`);
+
+  function mms(){
+      return setMessage("Let’s gooo!!🔥");
+  }
 
   return (
     <>
-      <h1>"Let's build something cool today!"</h1>
+      <h1>{message}</h1>
+      <button onClick={mms}>Change</button>
     </>
   )
 }
+
+
 
 export default App

d2799eb968cb35d00917da2c60b683d2cf29c693
Arthur Weale <ajayrossy340@gmail.com>
2025-11-13 01:38:32 +0200

    Added api fetch logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,57 +4,46 @@ import {Form} from './Form'
 
 
 
-function App() { 
-  
-  const [items, setItems] = useState(()=>{
-    const items = localStorage.getItem('items')
-    if(items === null) return []
+function App() {
+  const [todo, setTodo] = useState([]);
 
-    return JSON.parse(items)
-  });
-
-  function addFormElements(listItem){
-      setItems((currentItems) =>{
-      return [...currentItems, {id:crypto.randomUUID(), listItem, complete: false}]
-    })
-  }
-
-  useEffect(()=> {
-    localStorage.setItem('items', JSON.stringify(items))
-  }, [items])
-
-  function handleCheck(id, complete){
-    setItems(currentItems => {
-      return currentItems.map(item =>{
-        if(item.id === id){
-          return {...item, complete}
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await fetch("http://localhost:3000/");
+      try {
+        if (!response.ok) {
+          throw new Error("fetching todo failed");
         }
-        return item
-      })
-    })
-  }
+        const data = await response.json();
+        setTodo(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTodos();
+  }, []);
 
-  function HandleDelete(id){
-    setItems((currentItems)=>{
-      return currentItems.filter(item => (item.id !== id))
-    })
+  // function HandleDelete(id){
+  //   setItems((currentItems)=>{
+  //     return currentItems.filter(item => (item.id !== id))
+  //   })
 
-  }
-  
   return (
     <>
-      <Form formElements={addFormElements}/>
-      <ul >
-        {items.map(item => (
-          <li key={item.id}>
-            <input type="checkbox" checked={item.complete} onChange={event => handleCheck(item.id,event.target.checked )} />
-            {item.listItem} 
-            <button onClick={() => HandleDelete(item.id)}>Delete</button>
+      <Form />
+      <ul>
+        {todo.map((item) => (
+          <li key={item._id}>
+            <input type="checkbox" checked={item.complete} />
+            {item.todo}
+            <button>Delete</button>
           </li>
         ))}
       </ul>
     </>
-  )
+  );
+  //onChange={event => handleCheck(item.id,event.target.checked )}
+  //{/* <button onClick={() => HandleDelete(item.id)}>Delete</button> */}
 }
 
 

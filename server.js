6d8eb89f0475a4e0a6e5498ffb9244a2f9daff4a
Arthur Weale <ajayrossy340@gmail.com>
2025-11-15 01:00:12 +0200

    Added comments

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 import todoRoutes from "./src/routes/todoRoutes.js"
 
+//Reads enviroment variables and stores them in the process.env object.
 dotenv.config();
 
+//Initialised express and stores in app.
 const app = express();
 
+//Enables the frontend to communicate with the server despite the different urls.
 app.use(cors());
+
+//Middleware allows the server to parse json since express can't read json.
 app.use(express.json());
 
-// app.use("/", (req, res)=>{
-//     res.send("Hello I'm a todo app")
-// })
-
+//Routes from the base url
 app.use("/", todoRoutes)
 
 const port = process.env.PORT || 3000;

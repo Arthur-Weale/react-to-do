f9dd1a0b554e2177654e096aeef4d5a6b95fa087
Arthur Weale <ajayrossy340@gmail.com>
2025-11-15 01:01:22 +0200

    Added comments and removed unwanted code

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -1,49 +1,58 @@
 import initdb from "../models/db.js";
 import todoModel from "../models/schema.js"
 
+//Initially called initdb and establishes a database connection.
 await initdb();
 
 //Create
-export const insertTodo = async(formData)=>{
-    try {
-        await initdb();
-        //console.log(formData.name);
-        const result = await todoModel.create({
-            todo: formData.todo,
-            //completed: formData.completed //to change later....
-        })
-        return result
-    } catch (error) {
-        console.log(error)
-    }
-}
+export const insertTodo = async (formData) => {
+  try {
+    await initdb(); // initializes the database connection
+    const result = await todoModel.create({// creates a new todo document in the DB
+      todo: formData.todo, // sets the 'todo' field from formData
+      completed: formData.completed, // sets the 'completed' field from formData
+    });
+    return result; // returns the newly created todo
+  } catch (error) {
+    console.log(error); // logs any errors during DB operation
+  }
+};
+
 
 //Read
 export const getTodos = async()=> {
     try {
-        const result = await todoModel.find();
-        console.log(result);
-        return result;
+        const result = await todoModel.find();//Find the 'todos' from the database.
+        return result; //Returns the 'todos'.
     } catch (error) {
-        console.log(error);
+      console.log(error); // logs any errors during search operation
     }
 }
 
 //Update
-// const editTodo = async ()=>{
-//     try {
-        
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-// //Delete
-// const deleteTodo = async () => {
-//   try {
-//     await todoModel.deleteOne()
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
+export const editTodo = async (resultCheck, editedTodo) => {
+  try {
+    const updatedTodo = await todoModel.findByIdAndUpdate(editedTodo,{ //Finds the 'todo' to update by using the todo's id
+        completed: resultCheck.completed //Changes the the completed property.
+    },
+    {new: true})//This tells mongoose to return the updatedTodo and this a required arg.
+    return updatedTodo; //Returns the updated todo.
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-//export default {insertTodo, getTodos};
+//Delete
+export const deleteTodo = async (id) => {
+  try {
+    const deletedTodo = await todoModel.findByIdAndDelete(id)//Finds the todo id and delete the to-do with the matching id.
+    if(deletedTodo){ //A check to see if todo was deleted.
+        console.log("Todo was deleted");
+        return deleteTodo;
+    }else{
+        console.log("Failed to delete todo");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
